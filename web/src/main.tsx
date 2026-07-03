a161bb3301f282a54df134e0caa8bf454f5c87a9
kylian <kylian54000@gmail.com>
2026-07-03 05:06:21 +0200

    feat(web): relative base + webview bridge adapter (EXT Web M0)

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { onExtensionMessage } from './lib/vscode';
+import { useGraphStore } from './stores/useGraphStore';
+
+onExtensionMessage((msg) => {
+  if (msg.type === 'refresh') void useGraphStore.getState().fetchGraph();
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
